c74a2b7b403d0d81c167ed7b8b75184b68f1db00
Nanette <njulius89@gmail.com>
2018-01-09 22:00:48 -0800

    yourMusic page

---
+++ yourMusic.php
@@ -0,0 +1,12 @@
+<?php
+include("includes/includedFiles.php")
+?>
+
+<div class="playlistContainer">
+  <div class="gridViewContainer">
+    <h2>PLAYLISTS</h2>
+    <div class="buttonItems">
+      <button class="button green" type="button" name="button" onclick="createPlaylist('boo')">NEW PLAYLIST</button>
+    </div>
+  </div>
+</div>
