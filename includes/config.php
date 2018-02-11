b6f3d72afe442458d5019ea8bc837254157b5dbf
Nanette <njulius89@gmail.com>
2018-02-11 01:02:25 -0800

    bug

--- includes/config.php
+++ includes/config.php
@@ -4,16 +4,10 @@
   session_start();
   $timezone = date_default_timezone_set("America/Los_Angeles");
 
-  // $con = mysqli_connect("localhost", "root", "", "jamify");
-  //
-  // if(mysqli_connect_errno()) {
-  //   echo "Failed to connect: " . mysqli_connect_errno();
-  // }
+  $con = mysqli_connect("localhost", "root", "", "jamify");
 
-
-  if (is(get(get($process, "env"), "JAWSDB_URL"))) {
-  $con = call_method($mysql, "createConnection", get(get($process, "env"), "JAWSDB_URL"));
-} else {
-  $con = call_method($mysql, "createConnection", new Object("root", 3000.0, "host", "localhost", "user", "root", "password", "", "database", "jamify"));
-}
+  if(mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+  }
+  
 ?>
