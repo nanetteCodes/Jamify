1ee34ae7886c5d48b652be4f17e07115358a30e8
Nanette <njulius89@gmail.com>
2018-02-11 00:48:03 -0800

    deployment

--- includes/config.php
+++ includes/config.php
@@ -4,10 +4,16 @@
   session_start();
   $timezone = date_default_timezone_set("America/Los_Angeles");
 
-  $con = mysqli_connect("localhost", "root", "", "jamify");
+  // $con = mysqli_connect("localhost", "root", "", "jamify");
+  //
+  // if(mysqli_connect_errno()) {
+  //   echo "Failed to connect: " . mysqli_connect_errno();
+  // }
 
-  if(mysqli_connect_errno()) {
-    echo "Failed to connect: " . mysqli_connect_errno();
-  }
-  
+
+  if (is(get(get($process, "env"), "JAWSDB_URL"))) {
+  $con = call_method($mysql, "createConnection", get(get($process, "env"), "JAWSDB_URL"));
+} else {
+  $con = call_method($mysql, "createConnection", new Object("root", 3000.0, "host", "localhost", "user", "root", "password", "", "database", "jamify"));
+}
 ?>
